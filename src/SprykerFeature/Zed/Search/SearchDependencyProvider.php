9f9a89ab8cbf0f3a36394328689b255580ca5050
René Klatt <rene.klatt@spryker.com>
2015-10-08 09:33:13 +0000

    CD-520 refactored search and storage maintenance page

--- src/SprykerFeature/Zed/Search/SearchDependencyProvider.php
+++ src/SprykerFeature/Zed/Search/SearchDependencyProvider.php
@@ -7,9 +7,57 @@
 namespace SprykerFeature\Zed\Search;
 
 use SprykerEngine\Zed\Kernel\AbstractBundleDependencyProvider;
-use SprykerFeature\Zed\Search\Business\Model\SearchInstallerInterface;
+use SprykerEngine\Zed\Kernel\Container;
 
 class SearchDependencyProvider extends AbstractBundleDependencyProvider
 {
+
+    const CLIENT_SEARCH = 'search client';
+    const FACADE_COLLECTOR = 'collector facade';
+
+    /**
+     * @param Container $container
+     *
+     * @return Container
+     */
+    public function provideBusinessLayerDependencies(Container $container)
+    {
+        $this->addSearchClient($container);
+
+        return $container;
+    }
+
+    /**
+     * @param Container $container
+     *
+     * @return Container
+     */
+    public function provideCommunicationLayerDependencies(Container $container)
+    {
+        $this->addSearchClient($container);
+        $this->addCollectorFacade($container);
+
+        return $container;
+    }
+
+    /**
+     * @param Container $container
+     */
+    protected function addSearchClient(Container $container)
+    {
+        $container[self::CLIENT_SEARCH] = function (Container $container) {
+            return $container->getLocator()->search()->client();
+        };
+    }
+
+    /**
+     * @param $container
+     */
+    protected function addCollectorFacade($container)
+    {
+        $container[self::FACADE_COLLECTOR] = function (Container $container) {
+            return $container->getLocator()->collector()->facade();
+        };
+    }
 
 }

358316868c93287c16f1fac9eab25a9577f04b41
René Klatt <rene.klatt@spryker.com>
2017-10-09 09:58:27 +0000

    core-2001 Added new Frontend Module, added missing console commands to Modules

---
+++ src/Spryker/Zed/Search/Communication/Console/SearchDeleteIndexConsole.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Search\Communication\Console;
+
+use Spryker\Zed\Kernel\Communication\Console\Console;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method \Spryker\Zed\Search\Business\SearchFacade getFacade()
+ */
+class SearchDeleteIndexConsole extends Console
+{
+
+    const COMMAND_NAME = 'search:delete';
+    const DESCRIPTION = 'This command will delete the search index.';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDescription(self::DESCRIPTION);
+
+        parent::configure();
+    }
+
+    /**
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->getFacade()->getTotalCount() === 0) {
+            $this->info('Search index is empty');
+
+            return;
+        }
+
+        $this->info('Delete search index');
+        $this->getFacade()->delete();
+    }
+
+}
